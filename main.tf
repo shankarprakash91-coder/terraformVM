3bee1a5e92c5cf9f3d667a781c6936a84bc924d6
shivashankar <shankarprakash91@gmail.com>
2026-01-18 11:48:08 +0530

    adding all the required file for vm

--- main.tf
+++ main.tf
@@ -3,6 +3,32 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vm-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vm-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
@@ -12,7 +38,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_password      = var.admin_password
   disable_password_authentication = false
 
-  network_interface_ids = []
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 
   os_disk {
     caching              = "ReadWrite"
@@ -24,28 +52,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
-  }
-}
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vm-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-resource "azurerm_subnet" "subnet" {
-  name                 = "vm-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-resource "azurerm_network_interface" "nic" {
-  name                = "vm-nic"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
   }
 }
