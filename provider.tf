a0367eae6d08608d6238cd098b1697f5dee3331b
shankarprakash91-coder <shankarprakash91@gmail.com>
2026-01-20 09:18:00 +0530

    Add Terraform required providers for azurerm
    Add required providers block for Terraform configuration.

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.58"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 provider "azurerm" {
   features {}
 }
